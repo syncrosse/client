9cd3700c9e538415d75407fce1bc20339fb175c9
Devon Mack <devon.mack@shopify.com>
2021-08-07 17:54:58 -0400

    add default lobby

--- src/useSyncrosse.ts
+++ src/useSyncrosse.ts
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { Syncrosse } from './Syncrosse';
 import { Message } from './types';
 
-export function useSyncrosse() {
+export function useSyncrosse(lobbyId: string = '') {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [syncrosse] = useState<Syncrosse>(new Syncrosse('general'));
+  const [syncrosse] = useState<Syncrosse>(new Syncrosse(lobbyId));
 
   useEffect(() => {
     syncrosse.onEvent('chat', (message) => setMessages((oldMessages) => [...oldMessages, message]));
